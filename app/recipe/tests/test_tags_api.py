5c74cdf4d21abf1a45bf805e04f62ae9ddbd1c21
Muteshi <lumteshi@gmail.com>
2020-12-04 21:35:51 +0300

    Modified Tag view to add user

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -73,3 +73,25 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """
+        Test adding of  new tag
+        """
+        payload = {'name': 'Simple tag'}
+        self.client.post(TAGS_URL, payload)
+
+        tag_exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(tag_exists)
+
+    def test_create_tag_invalid(self):
+        """
+        Test creating a new tag with invalid information
+        """
+        payload = {'name': ''}  # tag with empty name
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
